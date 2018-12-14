f2d7caf352c0c837abc4c215bc91072db20bd654
Sakamoto <949487828@qq.com>
2018-12-14 15:17:32 +0800

    add

---
+++ 08  Webpack/src/main.js
@@ -0,0 +1,11 @@
+/* jshint esversion:6 */
+//上面的代码用于提示现在使用ES6语法（不然总是提示出错）
+
+import $ from 'jquery';
+
+$(function () {
+    $('li:odd').css('color', 'red');
+    $('li:even').css('color', function () {
+        return '#' + 'DA4396';
+    });
+});
---
+++ 08  Webpack/webpack.config.js
@@ -0,0 +1,11 @@
+/*jshint esversion:6*/
+
+const path = require('path');
+module.exports = {
+    entry: path.join(__dirname, './src/main.js'),
+    output: {
+        path: path.join(__dirname, './dist'),
+        filename: 'bundle.js'
+    },
+    mode: 'development'
+};
